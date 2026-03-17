0ce78b4137e7e5791377b97aa231a7d8acfce4e5
Anton Sadik <asadik@c3r2p5.42vienna.com>
2026-03-17 17:22:28 +0100

    Fixed out of bound write

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: asadik <asadik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 15:12:32 by asadik            #+#    #+#             */
-/*   Updated: 2026/03/17 17:11:04 by asadik           ###   ########.fr       */
+/*   Updated: 2026/03/17 17:21:34 by asadik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,7 @@ int	*parse_args(char **args, int *len)
 	if (len == 0)
 		exit(EXIT_SUCCESS);
 	numbers = ft_calloc(*len, sizeof(int));
-	if (!numbers)
-		return (numbers);
-	while (i < *len)
+	while (i < *len && numbers)
 	{
 		current = ft_atoi(args[i]);
 		if (current.type == ERROR)
@@ -70,7 +68,7 @@ char	**read_args(int argc, char **argv, bool *split)
 			args[i - 1] = argv[i];
 			i++;
 		}
-		args[i] = NULL;
+		args[i - 1] = NULL;
 	}
 	return (args);
 }
@@ -139,4 +137,5 @@ int	main(int argc, char **argv)
 		return (ft_printf("Error\n"), 0);
 	for (i = 0; i < len; i++)
 			ft_printf("%d\n", numbers[i]);
+	free(numbers);
 }
